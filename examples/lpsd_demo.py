867bb9beea460db56a0e7a98b006d50372e8237b
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2021-09-23 18:07:36 +0200

    Apply black code style to example.

--- examples/lpsd_demo.py
+++ examples/lpsd_demo.py
@@ -7,9 +7,9 @@ from scipy import signal
 
 def main():
     N = int(1e5)  # Number of data points in the timeseries
-    fs = 2.  # sampling rate
+    fs = 2.0  # sampling rate
     fmin = float(fs) / N  # lowest frequency of interest
-    fmax = float(fs) / 2.  # highest frequency of interest
+    fmax = float(fs) / 2.0  # highest frequency of interest
     Jdes = 1000  # desired number of points in the spectrum
     Kdes = 100  # desired number of averages
     Kmin = 2  # minimum number of averages
@@ -18,15 +18,15 @@ def main():
     X, f, C = lpsd(x, np.hanning, fmin, fmax, Jdes, Kdes, Kmin, fs, xi)
 
     # Compare to Pwelch
-    nfft = np.ceil(fs / float(fmin))
+    nfft = np.ceil(fs / fmin)
     window = np.hanning(nfft)
-    f_Pwelch, Pxx = signal.welch(x, fs, window, nfft=nfft, scaling='density')
-    
+    f_Pwelch, Pxx = signal.welch(x, fs, window, nfft=nfft, scaling="density")
+
     plt.figure()
     plt.loglog(f_Pwelch, Pxx)
-    plt.loglog(f, X*C['PSD'])
+    plt.loglog(f, X * C["PSD"])
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
