fdd66865cecfa963c98428ca9fec17947c2f72ca
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2021-09-28 14:41:28 +0200

    Move __init__.py to correct directory.

--- lpsd/__init__.py
+++ lpsd/__init__.py
@@ -3,4 +3,4 @@ from ._version import get_versions  # noqa: D104
 __version__ = get_versions()["version"]
 del get_versions
 
-from lpsd import lpsd  # noqa: F401
+from .lpsd import lpsd  # noqa: F401
