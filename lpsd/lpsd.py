6dc2208d4a990c87a8b4cc50ae4a80ab29c5eab1
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2021-09-28 11:16:08 +0200

    Set default values and make parameters more similar to scipy.signal.welch

--- lpsd/lpsd.py
+++ lpsd/lpsd.py
@@ -1,49 +1,50 @@
 import numpy as np
+from scipy.signal import get_window
 
 
-def lpsd(x, windowfcn, fmin, fmax, Jdes, Kdes, Kmin, fs, xi):
+def lpsd(x, fs, window, fmin, fmax, Jdes=1000, Kdes=100, Kmin=1, xi=0.5):
     """
-    Compute LPSD power spectrum estimation with a logarithmic frequency axis.
-
-    Estimates the power spectrum or power spectral density of the time series x at JDES
-    frequencies equally spaced (on a logarithmic scale) from FMIN to FMAX.
+    Compute the LPSD power spectrum estimation with a logarithmic frequency axis.
 
     Parameters
     ----------
     x : array_like
         time series to be transformed. "We assume to have a long stream x(n),
         n=0, ..., N-1 of equally spaced input data sampled with frequency fs. Typical
-        values for N range from 10^4 to >10^6" - Section 8 of [1]
-
-    windowfcn : callable
-        Function handle to windowing function (e.g. np.hanning) "Choose a window
-        function w(j, l) to reduce spectral leakage within the estimate. ... The
-        computations of the window function will be performed when the segment lengths
-        L(j) have been determined." - Section 8 of [1]
-
-    fmin, fmax : float
-        Lowest and highest frequency to estimate. "... we propose not to use the first
-        few frequency bins. The first frequency bin that yields unbiased spectral
-        estimates depends on the window function used. The bin is given by the effective
-        half-width of the window transfer function." - Section 7 of [1].
-
-    Jdes : int
-        Desired number of Fourier frequencies. "A typical value for J is 1000" - Section
-        8 of [1]
-
-    Kdes : int
-        Desired number of averages
-
-    Kmin : int
-        Minimum number of averages
+        values for N range from 10^4 to >10^6" [1]
 
     fs : float
-        Sampling rate
+        Sampling frequency of the `x` time series.
 
-    xi : float
-        Fractional overlap between segments (0 <= xi < 1). See Figures 5 and 6. "The
-        amount of overlap is a trade-off between computational effort and flatness of
-        the data weighting." [1]
+    window : str
+        Desired window to use. If `window` is a string or tuple, it is passed to
+        `scipy.signal.get_window` to generate the window values, which are DFT-even by
+        default. See `scipy.signal.get_window` for a list of windows and required
+        parameters. "Choose a window function w(j, l) to reduce spectral leakage within
+        the estimate. ... The computations of the window function will be performed when
+        the segment lengths L(j) have been determined." [1]
+
+    fmin, fmax : float, optional
+        Lowest and highest frequency to estimate. Defaults to `fs / len(x)` and the
+        Nyquist frequency `fs / 2`, respectively. "... we propose not to use the first
+        few frequency bins. The first frequency bin that yields unbiased spectral
+        estimates depends on the window function used. The bin is given by the effective
+        half-width of the window transfer function." [1].
+
+    Jdes : int, optional
+        Desired number of Fourier frequencies. Defaults to 1000. "A typical value for J
+        is 1000" [1]
+
+    Kdes : int, optional
+        Desired number of averages. Defaults to 100.
+
+    Kmin : int, optional
+        Minimum number of averages. Defaults to 1.
+
+    xi : float, optional
+        Fractional overlap between segments (0 <= xi < 1). Defaults to 0.5. "The
+         amount of overlap is a trade-off between computational effort and flatness of
+        the data weighting." [1]. See Figures 5 and 6 [1].
 
     Returns
     -------
@@ -75,8 +76,6 @@ def lpsd(x, windowfcn, fmin, fmax, Jdes, Kdes, Kmin, fs, xi):
     """
 
     # Sanity check the input arguments
-    if not callable(windowfcn):
-        raise TypeError("windowfcn must be callable")
     if not (fmax > fmin):
         raise ValueError("fmax must be greater than fmin")
     if not (Jdes > 0):
@@ -95,12 +94,10 @@ def lpsd(x, windowfcn, fmin, fmax, Jdes, Kdes, Kmin, fs, xi):
     N = len(x)  # Table 1
     jj = np.arange(Jdes, dtype=int)  # Table 1
 
-    if not (fmin >= fs / N):  # Lowest frequency possible
-        raise ValueError(
-            "The lowest possible frequency is {}, but fmin={}".format(fs / N, fmin)
-        )
-    if not (fmax <= fs / 2):  # Nyquist rate
-        raise ValueError("The Nyquist rate is {}, byt fmax={}".format(fs / 2, fmax))
+    if not fmin:
+        fmin = fs / N  # Lowest frequency possible
+    if not fmax:
+        fmax <= fs / 2  # Nyquist rate
 
     g = np.log(fmax) - np.log(fmin)  # (12)
     f = fmin * np.exp(jj * g / (Jdes - 1))  # (13)
@@ -158,19 +155,19 @@ def lpsd(x, windowfcn, fmin, fmax, Jdes, Kdes, Kmin, fs, xi):
         data -= np.mean(data, axis=0)  # (4) & (5)
 
         # Compute the discrete Fourier transform
-        window = windowfcn(L[jj])  # (5)
+        w = get_window(window, L[jj])  # (5)
         sinusoid = np.exp(
             -2j * np.pi * np.arange(L[jj])[:, np.newaxis] * m[jj] / L[jj]
         )  # (6)
-        data = data * (sinusoid * window[:, np.newaxis])  # (5,6)
+        data = data * (sinusoid * w[:, np.newaxis])  # (5,6)
 
         # Average the squared magnitudes
         Pxx[jj] = np.mean(np.abs(np.sum(data, axis=0)) ** 2)  # (8)
 
         # Calculate some properties of the window function which will be used during
         # calibration
-        S1[jj] = np.sum(window)  # (23)
-        S2[jj] = np.sum(window ** 2)  # (24)
+        S1[jj] = np.sum(w)  # (23)
+        S2[jj] = np.sum(w ** 2)  # (24)
 
     # Calculate the calibration factors
     C = {"PS": 2.0 / (S1 ** 2), "PSD": 2.0 / (fs * S2)}  # (28)  # (29)
